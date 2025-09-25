ee68e5669dbb2a76b9ec9f0b247f8ce9e135e75a
Shivam Tyagi <shivam.tyagi23123@gmail.com>
2025-09-24 23:27:12 -0700

    f8.c

--- f8.c
+++ f8.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 void count_adsc() {
-    char s[100]; int i,a=0,d=0,sp=0;
-    fgets(s,100,stdin);
-    for(i=0;s[i];i++)
-        if((s[i]>='a'&&s[i]<='z')||(s[i]>='A'&&s[i]<='Z')) a++;
-        else if(s[i]>='0'&&s[i]<='9') d++;
-        else if(s[i]!=' '&&s[i]!='\n') sp++;
-    printf("%d %d %d",a,d,sp);
+    char s[100]; int i = 0, a = 0, d = 0, sp = 0;
+    fgets(s, 100, stdin);
+    while (s[i]) {
+        if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z')) a++;
+        else if (s[i] >= '0' && s[i] <= '9') d++;
+        else if (s[i] != ' ' && s[i] != '\n') sp++;
+        i++;
+    }
+    printf("%d %d %d", a, d, sp);
 }
 
 int main() {
