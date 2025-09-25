2c05d4259396b103cb49e61cbac055ce1fb230dd
Shivam Tyagi <shivam.tyagi23123@gmail.com>
2025-09-24 23:26:54 -0700

    f7.c

--- f7.c
+++ f7.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
-int main() 
-{
-    char s[100]; int i,v=0,c=0;
-    fgets(s,100,stdin);
-    for(i=0;s[i];i++)
-        if((s[i]>='a'&&s[i]<='z')||(s[i]>='A'&&s[i]<='Z'))
-            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'
-            ||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U') v++;
-            else c++;
-    printf("%d %d",v,c);
-    
+void count_words() {
+    char s[100]; int i = 0, c = 0;
+    fgets(s, 100, stdin);
+    while (s[i]) {
+        if (s[i] != ' ' && s[i] != '\n' && (s[i+1] == ' ' || s[i+1] == '\n' || s[i+1] == 0))
+            c++;
+        i++;
+    }
+    printf("%d", c);
+}
+
+int main() {
+    count_words();
     return 0;
 }
