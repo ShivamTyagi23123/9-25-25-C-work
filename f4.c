0eae4d882674eada097d8086042d8278e09c81ff
Shivam Tyagi <shivam.tyagi23123@gmail.com>
2025-09-24 23:24:46 -0700

    f4.c

--- f4.c
+++ f4.c
@@ -1,14 +1,13 @@
- #include <stdio.h>
-#include <string.h>
+#include <stdio.h>
 
 int main() 
 {
-    char s[100]; int i,n,f=1;
-
+    char s[100]; int i, n=0, f=1;
     fgets(s,100,stdin);
-    n=strlen(s)-1;
+ 
+    while(s[n]!='\n'&&s[n]!=0) n++;
     for(i=0;i<n/2;i++) if(s[i]!=s[n-1-i]) f=0;
-    
     printf(f?"Palindrome":"Not Palindrome");
+ 
     return 0;
 }
