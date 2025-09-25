39c86619a6fad55736da981a5911c3aabd72128c
Shivam Tyagi <shivam.tyagi23123@gmail.com>
2025-09-24 23:27:35 -0700

    f9.c

--- f9.c
+++ f9.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 void swap_case(char s[]) {
-    int i;
-    for(i=0;s[i];i++)
-        if(s[i]>='a'&&s[i]<='z') s[i]=s[i]-32;
-        else if(s[i]>='A'&&s[i]<='Z') s[i]=s[i]+32;
-    printf("%s",s);
+    int i = 0;
+    while (s[i]) {
+        if (s[i] >= 'a' && s[i] <= 'z') s[i] -= 32;
+        else if (s[i] >= 'A' && s[i] <= 'Z') s[i] += 32;
+        i++;
+    }
+    printf("%s", s);
 }
 
 int main() {
     char s[100];
-    fgets(s,100,stdin);
+    fgets(s, 100, stdin);
     swap_case(s);
     return 0;
 }
