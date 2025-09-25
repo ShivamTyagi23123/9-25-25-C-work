73e06b328eea977b12831fd42078f24ff828a5e0
Shivam Tyagi <shivam.tyagi23123@gmail.com>
2025-09-24 23:29:00 -0700

    f1.c

--- f1.c
+++ f1.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-void count_words() {
-    char s[100]; int i, c=0;
-    fgets(s,100,stdin);
-    for(i=0;s[i];i++)
-        if((s[i]!=' '&&s[i]!='\n')&&((s[i+1]==' '||s[i+1]=='\n'||s[i+1]==0)))
-            c++;
-    printf("%d",c);
-}
 int main() {
-    count_words();
+    char s[100];
+    int i = 0, words = 0;
+    fgets(s, 100, stdin);
+    while (s[i]) {
+        if (s[i] != ' ' && s[i] != '\n' && (s[i + 1] == ' ' || s[i + 1] == '\n' || s[i + 1] == 0))
+            words++;
+        i++;
+    }
+    printf("%d", words);
     return 0;
 }
