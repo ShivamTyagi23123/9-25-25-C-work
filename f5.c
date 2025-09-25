7838ec0113e200b65df7b521304e69e16d4622b7
Shivam Tyagi <shivam.tyagi23123@gmail.com>
2025-09-24 23:25:07 -0700

    f5.c

--- f5.c
+++ f5.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() 
 {
-    char s[100],w[100],lw[100]; int i=0,j=0,max=0,len;
-
+    char s[100], lw[100]; int i=0, max=0, j=0, k;
     fgets(s,100,stdin);
-    for(;s[i];i++) {
-        if(s[i]!=' '&&s[i]!='\n') w[j++]=s[i];
+    while(s[i]) {
+        if(s[i]!=' '&&s[i]!='\n') lw[j++]=s[i];
         else {
-            w[j]=0; len=strlen(w);
-            if(len>max) { max=len; strcpy(lw,w);}
+            if(j>max) { max=j; lw[j]=0; }
             j=0;
         }
+        i++;
     }
+    lw[max]=0;
     printf("%s",lw);
+    
     return 0;
 }
